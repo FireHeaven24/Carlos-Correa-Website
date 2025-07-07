f1b5ae507c11c65eac4259e54eadef60e81e0cc8
Carlos Correa <74393830+FireHeaven24@users.noreply.github.com>
2025-07-07 04:36:52 -0400

    Comments added

--- script.js
+++ script.js
@@ -1,22 +1,141 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
+// let firstName = "Jonas";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let person = "jonas";
-let PI = 3.1415;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1994;
+// birthYear = 1990;
+
+// const job;
+
+// var job = "programmer";
+// job = "teacher";
+
+// lastName = 'Correa';
+// console.log(lastName);
+
+// const now = 2037;
+// const ageCarlos = now - 1991;
+// const ageAce = now - 2019;
+// console.log(ageCarlos * 2, ageAce / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Carlos";
+// const lastName = "Correa";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10; // x = x + 10
+// x += 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--;
+// x--;
+// console.log(x);
+
+// //comparison operators
+// console.log(ageCarlos > ageAce); // >, <, >=, <=
+// console.log(ageAce >= 18);
+
+// const isFullAge = ageAce >= 18;
+
+// console.log(now - 1991 > now - 2018);
+// const now = 2037;
+// const ageCarlos = now - 1994;
+// const ageAce = now - 1995;
+
+// console.log(now - 1994 > now - 1995);
+
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageCarlos + ageAce) / 2;
+// console.log(ageCarlos, ageAce, averageAge);
+
+// const firstName = "Carlos";
+// const job = "programmer";
+// const birthYear = 1994;
+// const year = 2025;
+
+// const Carlos =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// console.log(Carlos);
+
+// const carlosNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(carlosNew);
+
+// console.log(`Just a regular string...`);
+
+// console.log(
+//   "String with \n\
+//   multiple \n\
+//   lines"
+// );
+
+// console.log(`String
+//     multiple
+//     lines`);
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(
+    `Sarah is not old enough to drive. Come back in ${yearsLeft} years`
+  );
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
