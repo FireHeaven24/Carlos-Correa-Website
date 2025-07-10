5b1b4d652039a5d28f98e1af324259a56323b840
Carlos Correa <74393830+FireHeaven24@users.noreply.github.com>
2025-07-10 00:15:26 -0400

    Javascript file updated

--- script.js
+++ script.js
@@ -119,23 +119,109 @@
 //     multiple
 //     lines`);
 
-const age = 15;
+// const age = 15;
 
-if (age >= 18) {
-  console.log("Sarah can start driving");
+// if (age >= 18) {
+//   console.log("Sarah can start driving");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(
+//     `Sarah is not old enough to drive. Come back in ${yearsLeft} years`
+//   );
+// }
+
+// const birthYear = 2012;
+
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+//type conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(inputYear + 18);
+
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// //type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+//5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't speend it all");
+// } else {
+//   console.log("You should get a job");
+// }
+
+// let height = 123;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+// const age = 18;
+// if (age === 18) console.log("You just became an adult :3 (loose)");
+
+// if (age == 18) console.log("You just became an adult :3 (loose)");
+
+// const favorite = Number(prompt("What is your favorite number?"));
+// console.log(typeof favorite);
+
+// if (favorite === 23) {
+//   // 23 === 23
+//   console.log("Cool! 23 is an amazing nunmber!");
+// } else if (favorite === 7) {
+//   console.log("7 is also a cool number");
+// } else if (favorite === 9) {
+//   console.log("9 is also a cool number");
+// } else {
+//   console.log("Number is not 23 or 7 or 9");
+// }
+
+// if (favorite !== 23) console.log("why not 23?");
+
+const hasDriverLicense = true; //a
+const hasGoodVision = true; //b
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//   console.log("Sarah is able to drive");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; //c
+console.log(hasDriverLicense && hasGoodVision && isTired);
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive");
 } else {
-  const yearsLeft = 18 - age;
-  console.log(
-    `Sarah is not old enough to drive. Come back in ${yearsLeft} years`
-  );
+  console.log("Someone else should drive...");
 }
-
-const birthYear = 2012;
-
-let century;
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
-console.log(century);
