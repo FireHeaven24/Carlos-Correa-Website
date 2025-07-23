7052548925c4ac8d30fcbe563ef03f2bd0753c17
Carlos Correa <74393830+FireHeaven24@users.noreply.github.com>
2025-07-23 02:08:47 -0400

    JavaScript file update 2

--- script.js
+++ script.js
@@ -227,3 +227,12 @@
 // }
 
 const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+}
